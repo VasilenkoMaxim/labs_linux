4ddf9e6982d9f21d4713a85de2be7ca4b937f19c
Vasilenko Maxim <vasilenkonsk1992@gmail.com>
2017-07-13 16:12:35 +0700

    Добавлен комментарий в test.sh

--- lab8/test.sh
+++ lab8/test.sh
@@ -2,6 +2,6 @@
 
 TEGGREP="#mybackup"
 
-
+#Чатаем файл crontab и разделяем записи с тегом TEGGREP и остальные, и складываем все в разные файлы
 crontab -l | grep $TEGGREP > test0.txt
 crontab -l | grep -v $TEGGREP > test1.txt
